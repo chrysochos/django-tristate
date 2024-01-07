e76476993dc759b47bca740d9328d235295bc3f0
Ioannis Chrysochos <35873439+chrysochos@users.noreply.github.com>
2024-01-07 09:38:56 +0200

    First commit

--- tri/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TriConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "tri"
--- tria/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.http import HttpResponse
-from tria.views import my_view
-
-# URL Configuration
-urlpatterns = [
-    path('form/', my_view, name='form_view'),
-    path('success/', lambda request: HttpResponse("Form submitted successfully!")),
-]
